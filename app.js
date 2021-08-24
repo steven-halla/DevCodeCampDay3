8144d35a59660d96d37c8f9dbc4874d92c5835b0
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-08-24 16:53:19 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+// let i = ["one", "two", "three"];
+//
+// function myArray(array){
+//   for (let i = 0; i < array.length; i++) {
+//     console.log(array[0]);
+//   }
+// }
+//
+// myArray(i);
+//
+// function colorArray(array) {
+//   array.push("blue", "red", "white", "green", "yellow");
+//   console.log(array);
+//   const colorInput = prompt();
+//
+//   for (let i = 0; i < array.length; i++) {
+//     if (colorInput === array[i]) {
+//       console.log("yeah you got it");
+//       break;
+//     }
+//   }
+// }
+
+// colorArray([ "purple", "brown" ]);
+
+// function numberArray(array) {
+//   let sum = 0;
+//
+//   for ( let i = 0; i < array.length; i++) {
+//     sum = sum + array[i];
+//   }
+//   console.log(sum);
+//
+//   if ( sum % 2 === 0) {
+//     console.log("Numbers are even");
+//   }
+//   else if ( sum % 2 === 1) {
+//     console.log("Numbers are odd");
+//   }
+// }
+//
+// numberArray([1, 2, 4]);
+
+// function iterateArray(array, number) {
+//   for ( let i = 0; i < array.length; i++) {
+//     if (array[i] > number) {
+//       console.log(array[i]);
+//     }
+//   }
+// }
+//
+// iterateArray([2,5,7], 6);
+
+
+// UNFINISHED *******
+// MAYBE NEED TO DO A DOUBLE FOR LOOP  ON LINE  61
+// function oneArray(array) {
+//   let average = 0;
+//   let newArray = [];
+//   for ( let i = 0; i < array.length; i++) {
+//
+//     average += (array[i]);
+//     if (array[i] < average) {
+//       newArray.push(array[i]);
+//     }
+//   }
+//   average = average / array.length;
+//
+//   console.log(average);
+//   console.log(newArray);
+//
+//
+// }
+//
+// oneArray([2,4,9]);
+
+// *******************?
+
+//make edge case for numbers below 0
+
+// function noValue(array, number) {
+//     if (number >= array.length){
+//       console.log("no value here");
+//     } else {
+//       console.log(array[number]);
+//     }
+// }
+// noValue([1,2,4], 2)
+
+
